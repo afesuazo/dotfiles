f4db11de41ddd1d4fd1c55dced7e6e4bdf15247f
afesuazo <afesuazo@gmail.com>
2024-01-18 11:17:29 -0600

    Custom dashboard

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -6,28 +6,45 @@ return {
 
 	config = function()
 		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
+		local dashboard = require("alpha.themes.dashboard")
 
-		dashboard.section.header.val = {
-			[[                              ██                                 ]],
-			[[                             ██████                              ]],
-			[[                             ████████                            ]],
-			[[                        ███████████████                          ]],
-			[[                      ███████████████████                        ]],
-			[[                    ██████████████████████                       ]],
-			[[                   █████████████████████████                     ]],
-			[[           ██████████████████████████████████                    ]],
-			[[         ████████████████████████████████████                    ]],
-			[[           ██████                  ████████████                  ]],
-			[[                  █████                ████████                  ]],
-			[[                       ████              █████                   ]],
-			[[                         █████           ████                    ]],
-			[[                       █████████         ████                    ]],
-			[[                     ██████████████      ████                    ]],
-			[[                ███████████████████     ███                      ]],
-			[[             ██████████████████████     ██                       ]],
-			[[           ██████████████████████████  ██                        ]],
+		local logo = [[
+                                    ██                               
+			                             ██████                              
+			                             ████████                            
+			                        ███████████████                          
+			                      ███████████████████                        
+			                    ██████████████████████                       
+			                   █████████████████████████                     
+			           ██████████████████████████████████                    
+			         ████████████████████████████████████                    
+			           ██████                  ████████████                  
+			                  █████                ████████                  
+			                       ████              █████                   
+			                         █████           ████                    
+			                       █████████         ████                    
+			                     ██████████████      ████                    
+			                ███████████████████     ███                      
+			             ██████████████████████     ██                       
+			           ██████████████████████████  ██                        
+    ]]
+
+		local function getGreeting(name)
+			local datetime = os.date(" %Y-%m-%d   %H:%M:%S")
+			return "\t\t\t\t\t\t" .. "Hello " .. name .. "\n\t\t\t\t\t\t" .. datetime
+    end
+
+		local userName = "Strider"
+		local greeting = getGreeting(userName)
+		dashboard.section.header.val = vim.split(logo .. "\n" .. greeting, "\n")
+		dashboard.section.buttons.val = {
+			dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+			dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
+			dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
+			dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 		}
+
 		alpha.setup(dashboard.opts)
 	end,
 }
