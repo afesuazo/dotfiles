a01121903f28172563a1f442c38a81c1dffd6405
afesuazo <afesuazo@gmail.com>
2024-01-19 19:08:43 -0600

    Add cpp debugging plugin to nvim

---
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+{
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "tpope/vim-fugitive",
+    },
+    config = function(_, _)
+      vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
+      vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+      vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+      vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+    end,
+  },
+}
