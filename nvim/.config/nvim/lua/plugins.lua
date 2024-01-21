8085a693ca515d5e85eeb11f110b0569d485fa8c
afesuazo <afesuazo@gmail.com>
2024-01-20 20:54:17 -0600

    Theme

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,1 +1,16 @@
-return {}
+return {
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nightfox").setup({
+				options = {
+					transparent = true,
+					dim_inactive = true,
+				},
+			})
+			vim.cmd.colorscheme("dawnfox")
+		end,
+	},
+}
